b57526f7065f7a47c0e48f7fb89edde69c2d451c
Kustikphphph <puppy.maria@yandex.ru>
2024-11-13 18:55:20 +0300

    13.11.2024. ЛЗ-19. Турышев, Радышевский. Научились выполнять администрирование сайта,  заполнили таблицы БД информацией, используя административную панель,  не прибегая при этом к SQL-запросам.

--- WebBooks/WebBooks/settings.py
+++ WebBooks/WebBooks/settings.py
@@ -23,13 +23,14 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'catalog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog',
+    'django_cleanup',
 ]
 
 MIDDLEWARE = [
@@ -114,6 +115,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR/"static"),
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
