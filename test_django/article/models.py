89ca18121828a826eea602d56008ddc151aec829
Your Name <you@example.com>
2024-11-27 18:38:52 +0300

    27.11.2024. ЛЗ-24/ПЗ-24. Турышев, Радышевский. Научились создавать API для чтения данных.

--- test_django/article/models.py
+++ test_django/article/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
 
 class Author(models.Model): 
@@ -16,3 +16,11 @@ class Article(models.Model):
  def __str__(self): 
   return self.title
  
+User = get_user_model()
+class Capital(models.Model):
+    country = models.CharField('country', max_length=150)
+    capital_city = models.CharField('capital', max_length=150)
+    capital_population = models.IntegerField('population')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+      return self.capital_city
